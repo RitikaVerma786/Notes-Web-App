f63221cdb678978033bec42b54d49a7a5c9b240d
Ritika Verma <ritikaverma.vt@gmail.com>
2026-07-23 00:25:33 +0530

    jksf

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,5 +1,5 @@
 import styles from "./Search.module.css";
-import SearchIcon from "../../assets/search.png";
+import SearchIcon from "../../assets/Search.png";
 import { type SetStateAction } from "react";
 
 
